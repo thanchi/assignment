a612a7afb10ab87fd59b9732fafaa11271054a8c
Thanch <chian032@hotmail.com>
2016-04-27 16:25:58 +0700

    #06 เปลี่ยนภาพพื้นหลัง ^^

--- GamePanel.java
+++ GamePanel.java
@@ -1,22 +1,34 @@
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.image.*;
+import java.awt.image.BufferedImage;
 import java.util.ArrayList;
+import javax.imageio.ImageIO;
+import java.io.*;
+import java.io.File;
 
 public class GamePanel extends JPanel{
+	private Image image1;
 	private BufferedImage bi;
 	Graphics2D big;
 	ArrayList<Sprite> sprites = new ArrayList<Sprite>();
 	
 	public GamePanel(){
+		//image1 = new ImageIcon(getClass().getResource("pic1.jpg"));
 		bi = new BufferedImage(400 , 600 , BufferedImage.TYPE_INT_ARGB);
 		big = (Graphics2D) bi.getGraphics();
-		big.setBackground(Color.GRAY);
+		//big.setBackground(Color.GRAY);
+		try{
+			File file = new File ("pic1.jpg");
+			image1 = ImageIO.read(file);
+		}catch(IOException e){
+			e.printStackTrace();
+		}
 	}
 	
 	public void updateGameUI(GameReporter reporter){
 		big.clearRect(0, 0, 400, 600);
+		big.drawImage(image1, 0, 0, null);
 		big.setColor(Color.WHITE);
 		big.drawString(String.format("%08d", reporter.getScore()), 300, 20);
 		for(Sprite s : sprites){
